f0c8648e1d8969e65e98083e1cdd5a70f72a50b4
liucy2007975 <liuchunyin2007975@gmail.com>
2021-07-11 11:06:43 +0800

    u盘挂载监听

--- app/src/main/java/com/cow/liucy/box/ui/MainActivity.java
+++ app/src/main/java/com/cow/liucy/box/ui/MainActivity.java
@@ -1,12 +1,26 @@
 package com.cow.liucy.box.ui;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
+import android.os.Message;
 import android.support.annotation.Nullable;
+import android.support.v4.content.LocalBroadcastManager;
 
 import com.cow.liucy.face.R;
 import com.cow.liucy.libcommon.base.BaseActivity;
+import com.cow.liucy.libcommon.logger.AppLogger;
+import com.cow.liucy.libcommon.usbmonitor.Constant;
 
 public class MainActivity extends BaseActivity {
+
+    //==============声明变量=====================>>
+    private LocalBroadcastManager localBroadcastManager;
+    private IntentFilter intentFilter;
+    private LocalReceiver localReceiver;
+//===============注册/反注册====================>>
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -16,8 +30,40 @@ public class MainActivity extends BaseActivity {
     }
 
     @Override
+    protected void onResume() {
+        super.onResume();
+        localBroadcastManager = LocalBroadcastManager.getInstance(this);
+        intentFilter = new IntentFilter();
+        intentFilter.addAction(Constant.ACTION_USB_RECEIVER);
+        localReceiver = new LocalReceiver();
+        //注册本地接收器
+        localBroadcastManager.registerReceiver(localReceiver, intentFilter);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        localBroadcastManager.unregisterReceiver(localReceiver);
+    }
+
+    @Override
     protected void onDestroy() {
         super.onDestroy();
 
+    }
+
+    private class LocalReceiver extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String data = intent.getStringExtra("data");
+            Message msg = Message.obtain();
+            AppLogger.d( "收到本地广播==>>" + data);
+            if (data.equals("USB_MOUNT")) {
+                //msg.what = MyHandler.USB_MOUNT;
+                msg.obj = intent.getStringExtra("path");
+                AppLogger.d(  "收到本地广播=path=>>" +  msg.obj );
+            }
+            //mHandler.sendMessage(msg);
+        }
     }
 }
